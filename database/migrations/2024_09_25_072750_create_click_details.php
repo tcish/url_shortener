eb5496e19c4d8817b0c43ee5f5a13ab4dc09ea35
tcish <khaladkhan250@gmail.com>
2024-09-25 13:32:26 +0600

    added migrations

---
+++ database/migrations/2024_09_25_072750_create_click_details.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('click_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('url_id')->constrained('urls')->onDelete('cascade');
+            $table->string('ip_address')->nullable();
+            $table->timestamp('clicked_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('click_details');
+    }
+};
